dcd57f83244a4f38620d5ad6323c41b1d2c8c413
tjangoW <tjangoW@pc>
2021-10-25 15:09:02 +0100

    updated tmp files

--- computerSitTimer/tmp_multiprocessing.py
+++ computerSitTimer/tmp_multiprocessing.py
@@ -1,7 +1,67 @@
-def _main():
-    """easier to import locally in pycharm"""
+import os
+from multiprocessing import Process
+from time import sleep
+
+
+def info(title):
+    print(title)
+    print('module name:', __name__)
+    print('parent process:', os.getppid())
+    print('process id:', os.getpid())
+
+
+def f(name):
+    info('function f')
+    print('--> hello', name)
+    sleep(10)
+    print('--> end hello', name)
+
+
+def _main1_info():
+    info('main line')
+    p = Process(target=f, args=('bob',))
+    p.start()
+    p.join()
     pass
 
 
-if __name__ == "__main__":
-    _main()
+import multiprocessing as mp
+
+
+def foo(q):
+    q.put('hello')
+
+
+def _main2_mp():
+    ctx = mp.get_context('spawn')
+    q = ctx.Queue()
+    p = ctx.Process(target=foo, args=(q,))
+    p.start()
+    print(q.get())
+    p.join()
+    pass
+
+
+from multiprocessing import Process, Lock
+
+
+def f_lock(lock, i):
+    lock.acquire()
+    try:
+        print('hello world', i)
+    finally:
+        lock.release()
+        pass
+
+
+def _main3_lock():
+    lock = Lock()
+
+    for num in range(10):
+        Process(target=f_lock, args=(lock, num)).start()
+
+
+if __name__ == '__main__':
+    # _main1_info()
+    # _main2_mp()
+    _main3_lock()
